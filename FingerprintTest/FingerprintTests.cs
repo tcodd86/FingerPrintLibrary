eb3afb0625f9ed4d285f77f54a96fdc41d9732fb
Terrance Codd <tcodd86@msn.com>
2016-07-08 22:35:36 -0500

    Added unit test project and started writing tests.

---
+++ FingerprintTest/FingerprintTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FingerPrintLibrary;
+using System.Collections.Generic;
+
+namespace FingerprintTest
+{
+    [TestClass]
+    public class FingerprintTests
+    {
+        [TestMethod]
+        public void TestGetCheckSum()
+        {
+            //Arrange
+            var bytes = new List<byte> { 0x1, 0xe };
+
+            //Act
+            var sensor = new FingerPrintSensor("COM1");
+            var sum = sensor.AddCheckSum(bytes);
+
+            //Assert
+            //var expectedSum = new List<byte> { 0x1, 0xe, 0x0, 0xf };
+            Assert.AreSame(new byte[] { 0x1, 0xe, 0x0, 0xf }, sum);
+        }
+    }
+}
