b0dd65b614e34e3eee9f8f9d5cac59434cbc1b31
Terrance Codd <tcodd86@msn.com>
2016-07-21 19:35:16 -0500

    Refactored to move Serial Port code into its own wrapper class.

--- FingerPrintLibrary/FingerPrintSensor.cs
+++ FingerPrintLibrary/FingerPrintSensor.cs
@@ -9,16 +9,8 @@ namespace FingerPrintLibrary
 {
     public class FingerPrintSensor
     {
-        public int BaudRate { get; }
-
-        public string Address { get; }
-
-        private SerialPort Port { get; set; }
-
-        private bool Success { get; set; }
-
-        private TaskCompletionSource<byte[]> TCS = new TaskCompletionSource<byte[]>();
-
+        public SerialWrapper Wrapper;
+        
         #region Constructors
         /// <summary>
         /// Initializes new fingerprint sensor.
@@ -27,30 +19,7 @@ namespace FingerPrintLibrary
         /// <param name="address">Address of sensor.</param>
         public FingerPrintSensor(int baudRate, string address)
         {
-            if (baudRate % 9600 != 0 || baudRate < 9600 || baudRate > 115200)
-            {
-                throw new ArgumentOutOfRangeException("baudRate", "Baud rate must be a multiple of 9600 between 9600 and 115200");
-            }
-
-            BaudRate = baudRate;
-            Address = address;
-
-            SomeEvent += ReadFinished;
-
-            try
-            {
-                Port = new SerialPort(address, baudRate, Parity.None, 8);
-                Port.Open();
-
-                Port.DataReceived += new SerialDataReceivedEventHandler(Sensor_DataReceived);
-                //Minimum size of acknowledge packet is 12 bytes. Only trigger when 12 bytes have been received
-                Port.ReceivedBytesThreshold = 12;
-            }
-            catch (Exception ex)
-            {
-                var error = new Exception("Error while initializing serial port.", ex);
-                throw error;
-            }
+            Wrapper = new SerialWrapper(baudRate, address);
         }
 
         /// <summary>
@@ -62,27 +31,14 @@ namespace FingerPrintLibrary
         public FingerPrintSensor(string address) : this(57600, address)
         { }
         #endregion
-
-        private void WriteByteArray(byte[] write)
-        {
-            Port.Write(write, 0, write.Length);
-        }
-
-        private void WriteByteArray(byte[] write, int offset)
-        {
-            Port.Write(write, offset, write.Length - offset);
-        }
-
+        
         public bool HandShake()
         {
             var send = GenerateHandshakeInstruction();
 
-            var result = SendAndReadSerial(send).Result;
-
-            //parse result, return bool
+            var result = Wrapper.SendAndReadSerial(send).Result;
+            
             var success = ParseSuccess(result);
-            //test code only
-            //Port.Write(send, 0, send.Length);
 
             return success;
         }
@@ -97,56 +53,7 @@ namespace FingerPrintLibrary
             handshake.Add(0x0);
             return AddCheckSum(handshake);
         }
-
-        private void Sensor_DataReceived(object sender, SerialDataReceivedEventArgs args)
-        {
-            //max buffer length is 256. Padd a little.
-            int bufferSize = 300;
-            var buffer = new byte[bufferSize];
-
-            //maybe hardcode delay in here to make sure all bytes have been received
-            Port.Read(buffer, 0, Port.BytesToRead);
-
-            //Raise OnBufferFinished event
-            OnReadBufferFinished(new ReadFinishedEventArgs(buffer));
-        }
-
-        private event EventHandler<ReadFinishedEventArgs> SomeEvent;
-
-        private void OnReadBufferFinished(ReadFinishedEventArgs e)
-        {
-            SomeEvent?.Invoke(this, e);
-        }
-
-        private class ReadFinishedEventArgs
-        {
-            public byte[] ReadBuffer { get; set; }
-
-            public ReadFinishedEventArgs(byte[] readBuffer)
-            {
-                ReadBuffer = readBuffer;
-            }
-        }
         
-        private void ReadFinished(object sender, ReadFinishedEventArgs e)
-        {
-            TCS.SetResult(e.ReadBuffer);
-        }
-
-        private async Task<byte[]> SendAndReadSerial(byte[] sendData)
-        {
-            //start fresh
-            TCS = new TaskCompletionSource<byte[]>();
-
-            //send data to FingerPrint sensor
-            WriteByteArray(sendData);
-
-            await TCS.Task;
-
-            return TCS.Task.Result;
-        }
-        
-
         public int ParseLength(byte[] buffer)
         {
             throw new NotImplementedException();
@@ -250,16 +157,7 @@ namespace FingerPrintLibrary
         /// </summary>
         public void Disposeserial()
         {
-            if (Port.IsOpen)
-            {
-                Port.Close();
-            }
-            Port.Dispose();
-        }
-        
-        ~FingerPrintSensor()
-        {
-            Disposeserial();
+            Wrapper.Disposeserial();
         }
     }
 }
