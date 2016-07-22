cff10d5652cd63bd8e385e5ff017c137da154653
Terrance Codd <tcodd86@msn.com>
2016-07-21 19:16:30 -0500

    Added integration testing console app.

---
+++ FingerPrintTestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using FingerPrintLibrary;
+
+namespace FingerPrintTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the COM port the fingerprint sensor is on and press Enter.");
+
+            var comPort = Console.ReadLine();
+
+            Console.WriteLine("Looking for fingerprint sensor on port " + comPort + ".");
+
+            var sensor = new FingerPrintSensor(comPort);
+
+            var success = sensor.HandShake();
+
+            Console.WriteLine(success ? "Successfully connected to the fingerprint sensor." : "Failed to connect");
+
+            Console.ReadLine();
+        }
+    }
+}
